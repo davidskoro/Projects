9202b903392ee43223b4be8590bd40d55c988119
davidskoro <60477466+davidskoro@users.noreply.github.com>
2020-04-16 14:06:06 -0400

    Updated COVID-19 Analysis
    Converted datframes to csv

--- COVID-19 Analysis/Covid-19.py
+++ COVID-19 Analysis/Covid-19.py
@@ -26,21 +26,33 @@ daily = state.groupby('date').sum()
 daily['pct_cases'] = daily['cases'].pct_change()
 daily['pct_deaths'] = daily['deaths'].pct_change()
 daily['cfr'] = daily['deaths'] / daily['cases']
-
+daily.to_csv('output/daily.csv')
+                                
 # display values grouped by state
 statecfr = state.groupby('state').sum()
-statecfr['cfr'] = statecfr['deaths'] / statecfr['cases']
+statecfr['cfr'] = statecfr['deaths'] / statecfr['cases']           
+statecfr.to_csv('output/statecfr.csv')
+statecfr.sort_values('cfr')
+statecfr.sort_values('cfr', ascending=False)
 
 # display values grouped by county
 countycfr = county.groupby('county').sum()
 countycfr['cfr'] = countycfr['deaths'] / countycfr['cases']
+countycfr.to_csv('output/countycfr.csv')
+countycfr.sort_values('cfr', ascending=False)
 
 #%% Visualize the data
 
 # plot cases and deaths over time
-daily.plot.line(y=['cases','deaths'])
+dcd = daily.plot.line(y=['cases','deaths'])
+dcd.set(xlabel='Date',ylabel="Ammount", title="Cases and Deaths over Time")
+dcd.get_figure()
+dcd.get_figure().savefig('output/Time_Cases&Death.jpg', bbox_inches='tight')
 
 # plot case fatality rare over time 
-daily.plot.line(y='cfr')    
+cfr = daily.plot.line(y='cfr')
+cfr.set(xlabel='Date', ylabel='Ammount', title="Case Fataltity Rate over Time")
+cfr.get_figure()
+cfr.get_figure().savefig('output/Time_CFR.jpg', bbox_inches='tight')
 
 #%%
